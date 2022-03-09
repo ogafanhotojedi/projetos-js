5814f175cbce401d02b3c2249144b2a2b6a50dbf
O Gafanhoto Jedi <ogafanhotojedi@gmail.com>
2022-03-08 23:13:21 -0300

    mudei de tutorial
    o do programador BR não tava funcionando de jeito nenhum, então troquei de tutorial. por enquanto funciona, mas adicionando text e não imagens ao tabuleiro. tô commitando pra voltar aqui se for preciso

--- jogo-da-velha-2/js/scripts.js
+++ jogo-da-velha-2/js/scripts.js
@@ -1,6 +1,17 @@
-// definir os players
-const player1 = "X";
-const player2 = "O";
-var playTime = player1;
-var gameOver = false;
+const celulas = document.querySelectorAll(".celula");
+let checarVez = true;
+const JOGADOR_X = "X";
+const JOGADOR_O = "O";
 
+document.addEventListener("click", (event) => {
+    if(event.target.matches(".celula")){
+        jogar(event.target.id);
+    }
+});
+
+function jogar(id){
+    const celula = document.getElementById(id);
+    vez = checarVez ? JOGADOR_X : JOGADOR_O;
+    celula.textContent = vez;
+    checarVez = !checarVez;
+}
